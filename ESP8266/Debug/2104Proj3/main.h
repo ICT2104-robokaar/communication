9cbbd208364439048b019877a764475403c7580e
Claudia Tan Xu mei <2002145@sit.singaporetech.edu.sg>
2022-11-27 23:25:09 +0800

    Refactor Code
    Removed unnecessary files.

--- ESP8266/Debug/2104Proj3/main.h
+++
@@ -1,2 +0,0 @@
-float getPIDOutput();
-float getPIDOutput2();
--- UART.h
+++
@@ -1,5 +0,0 @@
-#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
-#include <Devices/ESP8266.h>
-#include <Hardware/UART_Driver.h>
-#include <string.h>
-#include <PWM.h>
